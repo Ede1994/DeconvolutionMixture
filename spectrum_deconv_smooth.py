bdfc42b8c112f59ab0ebf17ab29370c41aa75c1c
Eric Einspänner <60260090+Ede1994@users.noreply.github.com>
2021-01-27 15:29:09 +0100

    Add

--- spectrum_deconv_smooth.py
+++ spectrum_deconv_smooth.py
@@ -42,8 +42,8 @@ def callbackF(x):
 data_path_lu = 'C:/Users/Eric/Documents/GitHub/HalfLife/Data/Lu/10000Bq_20200916_300s.csv'
 data_path_iod = 'C:/Users/Eric/Documents/GitHub/HalfLife/Data/Iod/1000Bq_20201007_300s.csv'
 #data_path_mix = 'C:/Users/Eric/Documents/GitHub/HalfLife/Data/Mix/I-131_500Bq_Lu-177m_200Bq_300s_4.csv'
-data_path_mix = 'C:/Users/Eric/Documents/GitHub/HalfLife/Data/Mix2/AWM_MIX_100vs100_3600s.csv'
-#data_path_mix = 'C:/Users/Eric/Documents/GitHub/HalfLife/Data/Mix2/AWM_MIX_50vs97_3600s.csv'
+#data_path_mix = 'C:/Users/Eric/Documents/GitHub/HalfLife/Data/Mix2/AWM_MIX_100vs100_3600s.csv'
+data_path_mix = 'C:/Users/Eric/Documents/GitHub/HalfLife/Data/Mix2/AWM_MIX_50vs97_3600s.csv'
 #data_path_mix = 'C:/Users/Eric/Documents/GitHub/HalfLife/Data/Mix2/AWM_MIX_5vs86_3600s.csv'
 
 #%% read data
@@ -58,6 +58,8 @@ with open(data_path_lu, "r") as f:
             channels_lu.append(float(line[0]))
             counts_lu.append(float(line[1]))
 new_counts_lu = np.asarray([i/sum(counts_lu) for i in counts_lu])
+# Savitzky-Golay filter for smoothing
+new_counts_lu_smooth = savgol_filter(new_counts_lu, 21, 3) # window size, polynomial order
 
 channels_iod = []
 counts_iod = []
@@ -69,6 +71,8 @@ with open(data_path_iod, "r") as f:
             channels_iod.append(float(line[0]))
             counts_iod.append(float(line[1]))
 new_counts_iod = np.asarray([i/sum(counts_iod) for i in counts_iod])
+new_counts_iod_smooth = savgol_filter(new_counts_iod, 21, 3)
+
 
 channels_mix = []
 counts_mix = []
@@ -82,6 +86,7 @@ with open(data_path_mix, "r") as f:
             counts_mix.append(float(line[1]))
             bg_mix.append(float(line[2]))
 new_counts_mix = np.asarray(np.subtract(counts_mix, bg_mix))
+new_counts_mix_smooth = savgol_filter(new_counts_mix, 31, 3)
 
 #%% Converting channels to energies
 
@@ -111,9 +116,12 @@ print('{0:4s}       {1:9s}      {2:9s}       {3:9s}'.format('Iter', ' c_Lu', ' c
 
 # without smoothing
 xinit = np.array([0, 0])
-res = minimize(fun=obj_func, args=(counts_mix, new_counts_lu, new_counts_iod), x0=xinit, method='Nelder-Mead',\
-               tol=0.001, callback=callbackF, options={'maxiter':2000 ,'disp': True})
+#res = minimize(fun=obj_func, args=(counts_mix, new_counts_lu, new_counts_iod), x0=xinit, method='Nelder-Mead',\
+#               tol=0.001, callback=callbackF, options={'maxiter':2000 ,'disp': True})
 
+# with smoothing  
+res = minimize(fun=obj_func, args=(new_counts_mix_smooth, new_counts_lu_smooth, new_counts_iod_smooth), x0=xinit, method='Nelder-Mead',\
+               tol=0.001, callback=callbackF, options={'maxiter':2000 ,'disp': True})
 print('---------------------------')
 
 #%% Calculations
@@ -126,6 +134,8 @@ Lu_min, Lu_max = Lu_peak - 36, Lu_peak + 36
 Iod_peak = 791 #np.argmax(new_counts_iod)
 Iod_min, Iod_max = Iod_peak - 118, Iod_peak + 118
 
+print(sum(new_counts_mix_smooth[Iod_min:Iod_max]))
+
 print('\n--- Energy Windows ---')
 print('Lu: Peak {}, Min {}, Max {}'.format(Lu_peak, Lu_min, Lu_max))
 print('Iod: Peak {}, Min {}, Max {}'.format(Iod_peak, Iod_min, Iod_max))
@@ -133,8 +143,8 @@ print('---------------------')
 
 # results of normalization
 print('\n--- Normalization ---')
-print('Area Lu sopectrum (norm.):', round(simps(new_counts_lu, channels_lu),3))
-print('Area Iod spectrum (norm.):', round(simps(new_counts_iod, channels_iod),3))
+print('Area Lu sopectrum (norm.):', round(simps(new_counts_lu_smooth, channels_lu),3))
+print('Area Iod spectrum (norm.):', round(simps(new_counts_iod_smooth, channels_iod),3))
 print('---------------------')
 
 print('\n--- Optimized Factors ---')
@@ -143,20 +153,20 @@ print('c_Iod:', res.x[1])
 print('-------------------------')
 
 # Calculation of specific activities
-Lu_act = (simps((res.x[0]*new_counts_lu)[Lu_min:Lu_max], channels_lu[Lu_min:Lu_max]) / 3600) * 12.68
-Iod_act = (simps((res.x[1]*new_counts_iod)[Iod_min:Iod_max], channels_iod[Iod_min:Iod_max]) / 3600) * 18.7
+Lu_act = (simps((res.x[0]*new_counts_lu_smooth)[Lu_min:Lu_max], channels_lu[Lu_min:Lu_max]) / 3600) * 12.68
+Iod_act = (simps((res.x[1]*new_counts_iod_smooth)[Iod_min:Iod_max], channels_iod[Iod_min:Iod_max]) / 3600) * 16.5
 print('\n--- Calculated Activities ---')
 print('Lu activity [Bq]:', round(Lu_act , 2))
 print('Iod activity [Bq]:', round(Iod_act, 2))
 print('-----------------------------')
 
 # Calculation of the coefficient of determination (R^2)
-y_mean = sum(counts_mix)/float(len(counts_mix))
-ss_tot = sum((yi-y_mean)**2 for yi in counts_mix)
-ss_err = sum((yi-fi)**2 for yi,fi in zip(counts_mix,(res.x[0]*new_counts_lu+res.x[1]*new_counts_iod)))
+y_mean = sum(new_counts_mix_smooth)/float(len(new_counts_mix_smooth))
+ss_tot = sum((yi-y_mean)**2 for yi in new_counts_mix_smooth)
+ss_err = sum((yi-fi)**2 for yi,fi in zip(new_counts_mix_smooth,(res.x[0]*new_counts_lu_smooth+res.x[1]*new_counts_iod_smooth)))
 r2 = 1 - (ss_err/ss_tot)
 print('\n--- Coefficient Of Determination ---')
-print('R^2:', round(r2, 2))
+print('R^2:', round(r2, 3))
 print('------------------------------------')
 
 #%% plots
@@ -170,30 +180,33 @@ ax5 = fig.add_subplot(5,1,5)
 
 # Lu Spectrum (normalized)
 ax1.plot(np.asarray(energy_channels_lu), new_counts_lu, label='Lu Spectrum (normalized)')
+ax1.plot(np.asarray(energy_channels_lu), new_counts_lu_smooth, label='Lu Spectrum (smooth)')
 ax1.set_ylabel('x_i/sum(x_i)')
 ax1.legend()
 
 # Iod Spectrum (normalized)
 ax2.plot(np.asarray(energy_channels_iod), new_counts_iod, label='Iod Spectrum (normalized)')
+ax2.plot(np.asarray(energy_channels_iod), new_counts_iod_smooth, label='Iod Spectrum (smooth)')
 ax2.set_ylabel('x_i/sum(x_i)')
 ax2.legend()
 
 # Mixture (measured)
 ax3.plot(np.asarray(energy_channels_mix), counts_mix, label='Mixture (measured)')
+ax3.plot(np.asarray(energy_channels_mix), new_counts_mix_smooth, label='Mixture (smooth)')
 ax3.set_ylabel('counts')
 ax3.legend()
 
 # Lu + Iod (Calculated)
-ax4.plot(np.asarray(energy_channels_lu), res.x[0]*new_counts_lu, color='red', label='Lu (calculated)')
-ax4.plot(np.asarray(energy_channels_iod), res.x[1]*new_counts_iod, color='green', label='Iod (calculated)')
-ax4.plot(np.asarray(energy_channels_mix), (res.x[0]*new_counts_lu+res.x[1]*new_counts_iod), color='black', label='Lu + Iod')
+ax4.plot(np.asarray(energy_channels_lu), res.x[0]*new_counts_lu_smooth, color='red', label='Lu (calculated+smooth)')
+ax4.plot(np.asarray(energy_channels_iod), res.x[1]*new_counts_iod_smooth, color='green', label='Iod (calculated+smooth)')
+ax4.plot(np.asarray(energy_channels_mix), (res.x[0]*new_counts_lu_smooth+res.x[1]*new_counts_iod_smooth), color='black', label='Lu + Iod (smooth)')
 ax4.set_ylabel('counts')
 ax4.legend()
 
 # Diff
-ax5.plot(np.asarray(energy_channels_mix), counts_mix, label='Mixture (measured)')
-ax5.plot(np.asarray(energy_channels_mix), (res.x[0]*new_counts_lu+res.x[1]*new_counts_iod), color='black', label='Lu + Iod')
-ax5.plot(np.asarray(energy_channels_mix), (counts_mix-(res.x[0]*new_counts_lu+res.x[1]*new_counts_iod)), color='red', label='Diff')
+ax5.plot(np.asarray(energy_channels_mix), new_counts_mix_smooth, label='Mixture (smooth)')
+ax5.plot(np.asarray(energy_channels_mix), (res.x[0]*new_counts_lu_smooth+res.x[1]*new_counts_iod_smooth), color='black', label='Lu + Iod (smooth)')
+ax5.plot(np.asarray(energy_channels_mix), (new_counts_mix_smooth-(res.x[0]*new_counts_lu_smooth+res.x[1]*new_counts_iod_smooth)), color='red', label='Diff')
 ax5.set_xlabel('Energy (keV)')
 ax5.set_ylabel('counts')
 ax5.legend()
